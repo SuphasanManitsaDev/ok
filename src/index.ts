58d6735f6717266165691f14953e185af2eda655
SuphasanManitsaDev <suphasan.dev@gmail.com>
2025-08-28 11:09:41 +0700

    ok

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+import { Hono } from 'hono'
+import { serve } from '@hono/node-server'
+import { createNodeWebSocket } from '@hono/node-ws'
+
+const app = new Hono()
+
+const { injectWebSocket, upgradeWebSocket } = createNodeWebSocket({ app })
+
+app.get('/', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.get(
+  '/ws',
+  upgradeWebSocket((c) => {
+    return {
+      onMessage(event, ws) {
+        console.log(`Message from client: ${event.data}`)
+        ws.send('Hello from server!')
+      },
+      onClose: () => {
+        console.log('Connection closed')
+      },
+    }
+  })
+)
+
+const server = serve({
+  fetch: app.fetch,
+  port: 3000
+}, (info) => {
+  console.log(`Server is running on http://localhost:${info.port}`)
+})
+injectWebSocket(server)
